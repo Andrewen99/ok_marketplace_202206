409e79cdf01e40bfb1a38ec8c3a9469d97913a38
andrewshakirov <ewen9936@gmail.com>
2022-07-02 23:42:53 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "ok_marketplace_202206"
+
---
+++ src/test/kotlin/TestClass.kt
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test
+
+class TestClass {
+    @Test
+    fun baseTest() {
+        assert("animal".length == 6)
+    }
+}
